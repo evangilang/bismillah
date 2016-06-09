6a3459f3f8793ce4d8ff4f333f4496c2b50b432a
evangilang <evangilang96@email.unikom.ac.id>
2016-06-09 23:35:21 +0700

    first commit

---
+++ bismillahPlayground.playground/Contents.swift
@@ -0,0 +1,28 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+class Person {
+    var firstName: String
+    var lastName: String
+    
+    init(firstName: String, lastName: String){
+        self.firstName = firstName
+        self.lastName = lastName
+    }
+}
+
+class CollegeStudent: Person {
+    var schoolName: String
+    init(firstName: String, lastName: String, schoolName: String) {
+        self.schoolName = schoolName
+        super.init(firstName: firstName, lastName: lastName)
+    }
+    
+}
+
+let epan = Person(firstName: "Evan", lastName: "Gilang")
+print(epan.firstName)
+let aingUnikom = CollegeStudent(firstName: "evan", lastName: "gilang", schoolName: "UNIKOM")
+aingUnikom.schoolName
+aingUnikom.firstName
